76211c5dfb6ca8dd0d902fe188f036c4899f93b7
Vi Duong <vi.duong@siliconstack.com.au>
2023-09-26 15:10:45 +0700

    add: post request

--- BookBook.DTOs/DataTransferObject/CreateAuthorDto.cs
+++ BookBook.DTOs/DataTransferObject/CreateAuthorDto.cs
@@ -4,15 +4,23 @@ namespace BookBook.DTOs.DataTransferObject
 {
     public class CreateAuthorDto
     {
-        [Required(ErrorMessage = "Name is required")]
-        [StringLength(60, ErrorMessage = "Name can't be longer than 60 characters")]
-        public string? Name { get; set; }
+        [Required(ErrorMessage = "FirstName is required")]
+        [StringLength(50, ErrorMessage = "FirstName can't be longer than 50 characters")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "LastName is required")]
+        [StringLength(50, ErrorMessage = "LastName can't be longer than 50 characters")]
+        public string LastName { get; set; }
 
         [Required(ErrorMessage = "Date of birth is required")]
         public DateTime DayOfBirth { get; set; }
-        
+
         [Required(ErrorMessage = "Country is required")]
-        [StringLength(100, ErrorMessage = "Country cannot be loner then 100 characters")]
+        [StringLength(50, ErrorMessage = "Country cannot be loner then 50 characters")]
         public string? Country { get; set; }
+
+        [Required(ErrorMessage = "PhoneNumber is required")]
+        [StringLength(10, ErrorMessage = "PhoneNumber cannot be loner then 10 characters")]
+        public string? PhoneNumber { get; set; }
     }
 }
